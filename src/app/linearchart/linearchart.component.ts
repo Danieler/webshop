af65cb39413ecb1f23ab913affc5cb71dfd36eee
A178281 <desteban@externos.bankia.com>
2017-09-12 11:49:13 +0200

    tratamiento de datos

--- src/app/linearchart/linearchart.component.ts
+++ src/app/linearchart/linearchart.component.ts
@@ -10,23 +10,11 @@ import * as _ from "lodash";
 })
 export class LinearchartComponent {
 
-  constructor(public _cs:CouponsService){
-   _cs.Coupons.subscribe(
-     data => {
-       const mydata:any = data['coupons'];
-       console.log(_.filter(mydata, {webshop_id: 'macys'}));
-
-     }
-   );
-
-  }
   // lineChart
-  public lineChartData:Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
-  ];
-  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartData:Array<any> = [];
+
+  public lineChartLabels: Array<any> = [];
+
   public lineChartOptions:any = {
     responsive: true
   };
@@ -56,8 +44,47 @@ export class LinearchartComponent {
       pointHoverBorderColor: 'rgba(148,159,177,0.8)'
     }
   ];
-  public lineChartLegend:boolean = true;
-  public lineChartType:string = 'line';
+  public lineChartLegend: boolean = true;
+  public lineChartType: string = 'line';
+
+  constructor(public _cs: CouponsService){
+    _cs.Coupons.subscribe(
+      (data) => {
+        const mydata: any = data['coupons'];
+
+        var shops: any = _(data['coupons'])
+          .flatten()
+          .uniqBy('webshop_id')
+          .map(i => _.pick(i, 'webshop_id'))
+          .value();
+
+        shops = shops.map(function(shop) {
+          return shop.webshop_id
+        });
+
+        var couponsvalue: any = _(data['coupons'])
+          .flatten()
+          .uniqBy('value')
+          .map(i => _.pick(i, 'value'))
+          .value();
+
+        couponsvalue = couponsvalue.map(function(value) {
+          return value.value
+        });
+
+        this.lineChartLabels = _.sortBy(couponsvalue);
+
+        this.lineChartData.push({data: [65, 59, 80, 81, 56, 55, 40, 11, 60, 70, 33, 5, 40, 90, 60], label: shops[0]});
+        this.lineChartData.push({data: [28, 48, 40, 19, 86, 27, 90, 18, 48, 77, 9, 100, 27, 40, 70], label: shops[1]});
+        this.lineChartData.push({data: [18, 48, 77, 9, 100, 27, 40, 65, 59, 80, 81, 56, 55, 40, 11], label: shops[2]});
+
+
+        console.log(_.filter(mydata, {webshop_id: 'macys'}));
+
+      }
+    );
+
+  }
 
   public randomize():void {
     let _lineChartData:Array<any> = new Array(this.lineChartData.length);
@@ -68,7 +95,6 @@ export class LinearchartComponent {
       }
     }
     this.lineChartData = _lineChartData;
-    debugger;
   }
 
   // events
